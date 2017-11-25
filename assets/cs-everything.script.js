4fdf85ab4c13299fcab85c547d0f25ad1403802b
joeyaza <JAzadehdel@gmail.com>
2017-11-25 09:53:39 +0000

    updates as per mikeys message on 20th Nov

--- assets/cs-everything.script.js
+++ assets/cs-everything.script.js
@@ -1,79 +1,85 @@
 var touch = false, clickEv = 'click';
 
+if ($( "body" ).hasClass( "index-template" )) {
 
-var pages = 3;
-var currentpage = 1;
-if (document.location.hash) { currentpage = parseInt(document.location.hash.replace('#', '')); }
+  var pages = 3;
+  var currentpage = 1;
+  if (document.location.hash) { currentpage = parseInt(document.location.hash.replace('#', '')); }
 
-console.log(currentpage);
+  console.log(currentpage);
 
-var nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
-var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
-  
-var animatingup = false;
-var animatingdown = false;
-  
-$(document).ready(function() {
-  resizeDiv();
-  //scrolltocurrent();
-});
-
-window.onresize = function(event) {
-  resizeDiv();
-  scrolltocurrent();
-}
-
-$(window).scroll(function(event) {
-  
-  if (animatingup==true) { console.log("animating up..."); return; }
-  if (animatingdown==true) { console.log("animating down..."); return; }
-  
-  nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
-  prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
+  var nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
+  var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
     
-  console.log("scroll happened, previous page is " + prevpage + ", current page is " + currentpage + ", next page is " + nextpage);
+  var animatingup = false;
+  var animatingdown = false;
+    
+  $(document).ready(function() {
+    resizeDiv();
+    //scrolltocurrent();
+  });
 
-  //console.log($("#page"+(currentpage)).offset().top + " < " + $(window).scrollTop());
-  
-  //console.log($(window).scrollTop()+$(window).height() + " < " + $("#page"+(nextpage)).offset().top);
-  
+  window.onresize = function(event) {
+    resizeDiv();
+    scrolltocurrent();
+  }
 
-  if (animatingup == false) {
-    if ($(window).scrollTop()+$(window).height()>=$("#page"+(nextpage)).offset().top+50) {
-      if (nextpage > currentpage) {
-        var p2 = $( "#page"+(nextpage) );
-        var pageheight = p2.position().top;
-        animatingdown = true;
-        $('html, body').animate({ scrollTop: pageheight }, 400, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
-        return;
+  $(window).scroll(function(event) {
+    
+    if (animatingup==true) { console.log("animating up..."); return; }
+    if (animatingdown==true) { console.log("animating down..."); return; }
+    
+    nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
+    prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
+      
+    console.log("scroll happened, previous page is " + prevpage + ", current page is " + currentpage + ", next page is " + nextpage);
+
+    //console.log($("#page"+(currentpage)).offset().top + " < " + $(window).scrollTop());
+    
+    //console.log($(window).scrollTop()+$(window).height() + " < " + $("#page"+(nextpage)).offset().top);
+    
+
+    if (animatingup == false) {
+      if ($(window).scrollTop()+$(window).height()>=$("#page"+(nextpage)).offset().top+50) {
+        if (nextpage > currentpage) {
+          var p2 = $( "#page"+(nextpage) );
+          var pageheight = p2.position().top;
+          animatingdown = true;
+          $('html, body').animate({ scrollTop: pageheight }, 400, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
+          return;
+        }
       }
     }
-  }
-  
-  if (animatingdown == false) {
-    if ($(window).scrollTop()<=$("#page"+(currentpage)).offset().top-50) {
-      if (prevpage < currentpage) {
-        var p2 = $( "#page"+(currentpage) );
-        var pageheight = p2.position().top-$(window).height();
-        animatingup = true;
-        $('html, body').animate({ scrollTop: pageheight }, 400, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
-        return;
+    
+    if (animatingdown == false) {
+      if ($(window).scrollTop()<=$("#page"+(currentpage)).offset().top-50) {
+        if (prevpage < currentpage) {
+          var p2 = $( "#page"+(currentpage) );
+          var pageheight = p2.position().top-$(window).height();
+          animatingup = true;
+          $('html, body').animate({ scrollTop: pageheight }, 400, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
+          return;
+        }
       }
     }
+  });
+   
+
+  function scrolltocurrent() {
+    var p2 = $( "#page"+(currentpage) );
+    var pageheight = p2.position().top;
+    $('html, body').animate({ scrollTop: pageheight }, 200);
   }
-});
- 
 
-function scrolltocurrent() {
-  var p2 = $( "#page"+(currentpage) );
-  var pageheight = p2.position().top;
-  $('html, body').animate({ scrollTop: pageheight }, 200);
-}
+  function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $('.page').css({'min-height': vph + 'px'});
+  }
 
-function resizeDiv() {
-  vpw = $(window).width();
-  vph = $(window).height();
-  $('.page').css({'min-height': vph + 'px'});
+
+
+
 }
 
 
